844ad786d262accaf0c84e56407a36ed78724e99
YoussefEL-M <yoel0001@stud.dk>
2024-03-15 09:38:12 +0000

    Pls greg

--- src/main/java/com/example/testtest/TestTestApplication.java
+++ src/main/java/com/example/testtest/TestTestApplication.java
@@ -8,6 +8,7 @@ public class TestTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestTestApplication.class, args);
+	
+		System.out.println("Greg was here")
 	}
-
 }
